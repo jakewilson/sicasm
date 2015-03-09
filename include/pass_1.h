a2b3e46f0467e3e0bc24913099a7e5a4e5655de0
Jake Wilson <jakewilsonfl@gmail.com>
2015-03-08 23:18:31 -0400

    Adds files for pass 1.

---
+++ include/pass_1.h
@@ -0,0 +1,7 @@
+/*
+ * pass_1.h
+ * Header file for all Pass 1 related functions and macros.
+ *
+ * Author: Jake Wilson
+ * Date: March 8, 2015
+ */
---
+++ src/pass_1.c
@@ -0,0 +1,9 @@
+/*
+ * pass_1.c
+ * File for the functions of Pass 1 of the assembler.
+ *
+ * Author: Jake Wilson
+ * Date: March 8, 2015
+ */
+
+
