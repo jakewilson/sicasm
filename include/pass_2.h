d62ddb7afb2d37a6338ba6370d0eb4651b4cf12c
Jake Wilson <jakewilsonfl@gmail.com>
2015-03-27 22:25:09 -0400

    Adds pass 2 files.

---
+++ include/pass_2.h
@@ -0,0 +1,6 @@
+/*
+ * Header file for pass 2 of SIC/XE.
+ *
+ * Author: Jake Wilson
+ * Date: March 27, 2015
+ */
---
+++ src/pass_2.c
@@ -0,0 +1,6 @@
+/*
+ * Source file for pass 2 of SIC/XE.
+ *
+ * Author: Jake Wilson
+ * Date: March 27, 2015
+ */
